02bc5c0a2ecbda8b5e5e3e5b666d7086bc360fdc
diogo <dgssantos@hotmail.com>
2020-08-26 15:39:44 -0300

    change Authentication to receive a Model

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -3,8 +3,17 @@ import 'package:meta/meta.dart';
 import '../entities/entities.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth({
-    @required String username,
-    @required String password,
+  Future<AccountEntity> auth({AuthenticationParams params});
+}
+
+class AuthenticationParams {
+  final String username;
+
+  /// Sema thing as password
+  final String secret;
+
+  AuthenticationParams({
+    @required this.username,
+    @required this.secret,
   });
 }
