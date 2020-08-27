c13396e9c24219fd164f2f90af878c4d13868a73
diogo <dgssantos@hotmail.com>
2020-08-26 22:16:30 -0300

    move HttpAdapter to its  own file

---
+++ lib/infra/http/http_adatper.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:meta/meta.dart';
+
+import '../../data/http/http.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<Map<String, dynamic>> request({
+    @required String url,
+    @required String method,
+    Map<String, dynamic> body,
+  }) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    final response = await this.client.post(
+          url,
+          headers: headers,
+          body: jsonBody,
+        );
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else {
+      return null;
+    }
+  }
+}
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,40 +1,10 @@
-import 'dart:convert';
-
 import 'package:faker/faker.dart';
-import 'package:flutter_clean_study/data/http/http.dart';
+import 'package:flutter_clean_study/infra/http/http.dart';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
-import 'package:meta/meta.dart';
 
 import 'package:mockito/mockito.dart';
-
-class HttpAdapter implements HttpClient {
-  final Client dio;
-
-  HttpAdapter(this.dio);
-
-  Future<Map<String, dynamic>> request({
-    @required String url,
-    @required String method,
-    Map<String, dynamic> body,
-  }) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json',
-    };
-    final jsonBody = body != null ? jsonEncode(body) : null;
-    final response = await this.dio.post(
-          url,
-          headers: headers,
-          body: jsonBody,
-        );
-    if (response.statusCode == 200) {
-      return response.body.isEmpty ? null : jsonDecode(response.body);
-    } else {
-      return null;
-    }
-  }
-}
 
 class ClientSpy extends Mock implements Client {}
 
