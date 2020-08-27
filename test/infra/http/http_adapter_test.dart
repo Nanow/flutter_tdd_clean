878d620d558f369c43485fdc795cda4e48b21b25
diogo <dgssantos@hotmail.com>
2020-08-26 22:06:03 -0300

    move duplicated code to setUp

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -46,10 +46,25 @@ void main() {
   });
 
   group('post', () {
+    PostExpectation mockRequest() => when(
+          client.post(any,
+              headers: anyNamed('headers'), body: anyNamed('body')),
+        );
+
+    void mockResponse(
+      int statusCode, {
+      String body = '{"any_key":"any_value"}',
+    }) {
+      return mockRequest().thenAnswer(
+        (_) async => Response(body, statusCode),
+      );
+    }
+
+    setUp(() {
+      mockResponse(200);
+    });
+
     test('Should call post  with  correct values', () {
-      when(
-        client.post(any, headers: anyNamed('headers'), body: anyNamed('body')),
-      ).thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
       sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
       verify(
@@ -62,10 +77,6 @@ void main() {
       );
     });
     test('Should call post  without body', () {
-      when(client.post(
-        any,
-        headers: anyNamed('headers'),
-      )).thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
       sut.request(url: url, method: 'post');
 
       verify(
@@ -74,21 +85,13 @@ void main() {
     });
 
     test('Should return data if post returns 200', () async {
-      when(client.post(
-        any,
-        headers: anyNamed('headers'),
-      )).thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
-
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, {'any_key': 'any_value'});
     });
 
     test('Should returns 200 if post returns 200 with no data', () async {
-      when(client.post(
-        any,
-        headers: anyNamed('headers'),
-      )).thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
+      mockResponse(200, body: '');
 
       final response = await sut.request(url: url, method: 'post');
 
