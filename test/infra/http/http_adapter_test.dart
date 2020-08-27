3e28e480d37fded284ad42e05e65b9c601a4d5e2
diogo <dgssantos@hotmail.com>
2020-08-26 21:37:20 -0300

    ensure HttpAdapter calls post without correct Body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -21,7 +21,12 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
-    await this.dio.post(url, headers: headers, body: jsonEncode(body));
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await this.dio.post(
+          url,
+          headers: headers,
+          body: jsonBody,
+        );
   }
 }
 
@@ -49,6 +54,13 @@ void main() {
               'accept': 'application/json',
             },
             body: '{"any_key":"any_value"}'),
+      );
+    });
+    test('Should call post  without body', () {
+      sut.request(url: url, method: 'post');
+
+      verify(
+        client.post(any, headers: anyNamed('headers')),
       );
     });
   });
