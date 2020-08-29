102e7446ca0a66df1e5a71037030537ee4395845
diogo <dgssantos@hotmail.com>
2020-08-29 17:36:13 -0300

    move valiidation to  its own fiel

--- test/ui/presentation/stream_login_presentation_test.dart
+++ test/ui/presentation/stream_login_presentation_test.dart
@@ -1,13 +1,10 @@
 import 'dart:async';
 
+import 'package:mockito/mockito.dart';
 import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-
-abstract class Validation {
-  String validate({@required String field, @required String value});
-}
+import 'package:flutter_clean_study/presentation/protocols/validation.dart';
 
 class LoginState {
   String emailError;
