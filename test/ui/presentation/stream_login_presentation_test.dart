14a55efefb25742b5c090bd18433de0ca91a1a83
diogo <dgssantos@hotmail.com>
2020-08-29 16:57:27 -0300

    ensutre StreamLoginPresenter emits  error if login is invalid

--- test/ui/presentation/stream_login_presentation_test.dart
+++ test/ui/presentation/stream_login_presentation_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,12 +9,28 @@ abstract class Validation {
   String validate({@required String field, @required String value});
 }
 
+class LoginState {
+  String emailError;
+}
+
 class StreamLoginPresenter {
   final Validation validation;
 
+  final _controller = StreamController<LoginState>.broadcast();
+
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
+
+  var _state = LoginState();
+
   StreamLoginPresenter({@required this.validation});
   void validateEmail(String email) {
-    validation.validate(field: 'email', value: email);
+    _state.emailError = validation.validate(field: 'email', value: email);
+    _controller.add(_state);
+  }
+
+  dispose() {
+    _controller.close();
   }
 }
 
@@ -31,5 +49,18 @@ main() {
   test('Should  call Validation with correct email', () {
     sut.validateEmail(email);
     verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+
+  test('Should emit email error if validation fails', () {
+    when(
+      validation.validate(
+        field: anyNamed('field'),
+        value: anyNamed('value'),
+      ),
+    ).thenReturn('error');
+
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validateEmail(email);
   });
 }
