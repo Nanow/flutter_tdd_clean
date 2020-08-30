9a9521608cf9cde02f4d0e5ae1007aec71ea9c62
diogo <dgssantos@hotmail.com>
2020-08-30 12:37:39 -0300

    ensure ValidateComposite returns the first eror of the correct field

--- test/validation/validators/validation_composit_test.dart
+++ test/validation/validators/validation_composit_test.dart
@@ -13,7 +13,7 @@ class ValidationComposite implements Validation {
   @override
   String validate({@required String field, @required String value}) {
     String error;
-    for (final validation in _validations) {
+    for (final validation in _validations.where((v) => field == v.field)) {
       error = validation.validate(value);
       if (error != null && error.isNotEmpty) {
         return error;
@@ -45,13 +45,13 @@ main() {
 
   setUp(() {
     validation1 = FieldValidationSpy();
-    when(validation1.field).thenReturn('any_field');
+    when(validation1.field).thenReturn('other_field');
     mockValidation1(null);
     validation2 = FieldValidationSpy();
     when(validation2.field).thenReturn('any_field');
     mockValidation2(null);
     validation3 = FieldValidationSpy();
-    when(validation3.field).thenReturn('other_field');
+    when(validation3.field).thenReturn('any_field');
     mockValidation3(null);
     sut = ValidationComposite([validation1, validation2, validation3]);
   });
@@ -66,6 +66,6 @@ main() {
     mockValidation3('error_3');
     final error = sut.validate(field: 'any_field', value: 'any_value');
 
-    expect(error, 'error_1');
+    expect(error, 'error_2');
   });
 }
