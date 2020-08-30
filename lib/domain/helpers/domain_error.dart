34a089f47c0e9c6c0c359f6273a91ec7a1531b4b
diogo <dgssantos@hotmail.com>
2020-08-29 21:10:03 -0300

    ensrue StreamLoginPresenter emits correct events on UnexpectedError

--- lib/domain/helpers/domain_error.dart
+++ lib/domain/helpers/domain_error.dart
@@ -4,7 +4,9 @@ extension DomainErrorExtension on DomainError {
   String get description {
     switch (this) {
       case DomainError.invalidCredentails:
-        return "Credenciais inválidas";
+        return "Credenciais inválidas.";
+      case DomainError.unexpected:
+        return "Algo errado aconteceu. Tente novamente em breve.";
       default:
         return '';
     }
--- test/ui/presentation/stream_login_presentation_test.dart
+++ test/ui/presentation/stream_login_presentation_test.dart
@@ -150,14 +150,14 @@ main() {
     await sut.auth();
   });
 
-  test('Should emit correct events on InvalidCredentialsError', () async {
-    mockAuthenticationError(DomainError.invalidCredentails);
+  test('Should emit correct events on UnexpectedError', () async {
+    mockAuthenticationError(DomainError.unexpected);
     sut.validateEmail(email);
     sut.validatePassword(password);
     expectLater(sut.isLoadingStream, emits(false));
 
-    sut.mainErrorStream.listen(
-        expectAsync1((error) => expect(error, 'Credenciais inválidas')));
+    sut.mainErrorStream.listen(expectAsync1((error) =>
+        expect(error, 'Algo errado aconteceu. Tente novamente em breve.')));
     await sut.auth();
   });
 }
