04a6a4b8e1d4d339bdee17c3bf430c0cd5fb9586
diogo <dgssantos@hotmail.com>
2020-08-29 21:45:19 -0300

    ensure RequiredFieldValidation returns null if value is  empty

--- test/validation/validators/required_field_validation_test.dart
+++ test/validation/validators/required_field_validation_test.dart
@@ -12,6 +12,7 @@ class RequiredFieldValidation implements FieldValidation {
 
   @override
   String validate(String value) {
+    if (value.isEmpty) return 'Campo obrigatório';
     return null;
   }
 }
@@ -21,5 +22,10 @@ main() {
     final sut = RequiredFieldValidation(field: 'any field');
     final error = sut.validate('any_value');
     expect(error, null);
+  });
+  test('Should return error if value is empty', () {
+    final sut = RequiredFieldValidation(field: 'any field');
+    final error = sut.validate('');
+    expect(error, 'Campo obrigatório');
   });
 }
